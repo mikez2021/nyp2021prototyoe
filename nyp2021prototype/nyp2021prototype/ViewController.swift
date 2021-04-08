640a4b84150dde740de73d0844f779fb5fec9323
MikeZ <michaelinsg@gmail.com>
2021-04-08 11:39:38 +0800

    just created
    just created

---
+++ nyp2021prototype/nyp2021prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nyp2021prototype
+//
+//  Created by Specialist_Diploma on 8/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nyp2021prototype/nyp2021prototypeTests/nyp2021prototypeTests.swift
@@ -0,0 +1,33 @@
+//
+//  nyp2021prototypeTests.swift
+//  nyp2021prototypeTests
+//
+//  Created by Specialist_Diploma on 8/4/21.
+//
+
+import XCTest
+@testable import nyp2021prototype
+
+class nyp2021prototypeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
